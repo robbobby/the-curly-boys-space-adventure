8650afeb3e1d7f5d8db70c97c2f0df3968226417
Mateusz <mateusz.harlos@gmail.com>
2020-12-06 12:42:18 +0000

    miles convertor active on click

--- client/src/components/PlanetDetail.vue
+++ client/src/components/PlanetDetail.vue
@@ -1,13 +1,16 @@
 <template>
     <div id="selected-planet" v-if="planet">
         <h2>Planet {{planet.englishName}} </h2>
-        <p>Avrage Distance from Sun: {{planet.semimajorAxis}} km ({{milesConvertor(planet.semimajorAxis)}} miles) </p>
+        <p v-on:click="convertDistance = !convertDistance" v-show="convertDistance">Avrage Distance from Sun: {{planet.semimajorAxis}} km</p>
+        <p v-on:click="convertDistance = !convertDistance" v-show="!convertDistance">Avrage Distance from Sun: {{milesConvertor(planet.semimajorAxis)}} miles </p>
         <p>Time to Orbit Sun (a year): {{planet.sideralOrbit}} days </p>
         <p>Time to Spin on Axis (a day): {{planet.sideralRotation}} hours </p>
-        <p>Avrage Radius: {{planet.meanRadius}} km ({{milesConvertor(planet.meanRadius)}} miles) </p>
+        <p v-on:click="convertDistance = !convertDistance" v-show="convertDistance">Avrage Radius: {{planet.meanRadius}} km </p>
+        <p v-on:click="convertDistance = !convertDistance" v-show="!convertDistance">Avrage Radius: {{milesConvertor(planet.meanRadius)}} miles </p>
         <p>Density: {{planet.density}} g/cm³ </p>
         <p>Gravity: {{planet.gravity}} m/s² </p>
-        <p>Escape Velocity: {{(planet.escape)/1000}} km/s ({{milesConvertor(planet.escape)/1000}} mi/s) </p>
+        <p v-on:click="convertDistance = !convertDistance" v-show="convertDistance">Escape Velocity: {{(planet.escape)/1000}} km/s </p>
+        <p v-on:click="convertDistance = !convertDistance" v-show="!convertDistance">Escape Velocity: {{milesConvertor(planet.escape)/1000}} mi/s </p>
         <moon-list v-if="getMoons" :getMoons="getMoons"></moon-list>
     </div>
 </template>
@@ -18,6 +21,11 @@ import MoonList from './MoonList.vue'
 export default {
     name: 'planet-detail',
     props: ['planet', 'getMoons'],
+    data() {
+        return {
+            convertDistance: true
+        }
+    },
     components: {
         'moon-list': MoonList
     },
