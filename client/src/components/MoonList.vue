15c7e2314f8686e927d6be6b86694c275b6a22ca
Mateusz <mateusz.harlos@gmail.com>
2020-12-06 12:55:48 +0000

    moon counter added

--- client/src/components/MoonList.vue
+++ client/src/components/MoonList.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h3 v-on:click="showMoons = !showMoons">Moons:</h3>
+        <h3 v-on:click="showMoons = !showMoons">Moons ({{numberOfMoons()}}):</h3>
         <ul v-show="showMoons">
             <li v-for="(moon, index) in getMoons" :key="index">
                 <p> {{moon.englishName}} </p>
@@ -16,6 +16,11 @@ export default {
     data(){
         return {
             showMoons: false
+        }
+    },
+    methods: {
+        numberOfMoons: function(){
+            return Object.keys(this.getMoons).length
         }
     }
 }
